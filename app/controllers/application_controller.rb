14d2df7e8e8c009186011709ad497e82a8f5a894
Amirol Ahmad <9w2pdf@gmail.com>
2016-02-01 01:03:59 +0800

    false back to previous devise parameter behaviour

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:callsign, :email, :password, :password_confirmation, :remember_me)
-    end
-
-    devise_parameter_sanitizer.permit(:sign_in) do |u|
-      u.permit(:login, :callsign, :email, :password, :remember_me)
-    end
-
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:callsign, :email, :password, :password_confirmation, :current_password)
-    end
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:callsign, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :callsign, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:callsign, :email, :password, :password_confirmation, :current_password) }
   end
 end
