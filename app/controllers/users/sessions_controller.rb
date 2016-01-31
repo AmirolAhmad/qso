925631e83a04ef31115d8714c65053c884cd5aa3
Amirol Ahmad <9w2pdf@gmail.com>
2016-02-01 01:23:55 +0800

    redirect user to dashboard after login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -22,4 +22,7 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.for(:sign_in) << :attribute
   # end
+  def after_sign_in_path_for(resource_or_scope)
+    dashboard_path
+  end
 end
