60309b36f986bcbc9b643fa457f9dd40d594aaf4
Amirol Ahmad <9w2pdf@gmail.com>
2016-02-01 01:15:04 +0800

    configure custom devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    omniauth: 'users/omniauth',
+    unlocks: 'users/unlocks'
+  }
   devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy'
-    get '/login' => 'devise/sessions#new'
-    get '/register' => 'devise/registrations#new'
-    get '/settings' => 'devise/registrations#edit'
+    delete 'sign_out', :to => 'users/sessions#destroy'
+    get '/login' => 'users/sessions#new'
+    get '/register' => 'users/registrations#new'
+    get '/settings' => 'users/registrations#edit'
   end
 
   namespace :admin, path: '/master' do
